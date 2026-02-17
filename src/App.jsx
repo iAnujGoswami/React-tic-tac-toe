1de7a9ab63d23c0bf376edadf80433eda7bd9a58
Anuj <Anuj Goswami>
2026-02-17 13:54:25 +0530

    Reducing State Management and identiying unnecessary state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import {useState} from 'react';
 
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log"
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer='X';
+  if (gameTurns.length>0 && gameTurns[0].player==='X'){
+    currentPlayer='O';
+  }
+  return currentPlayer
+}
 
 function App() {
-  const [activePlayer, setActivePlayer]=useState('X');
-
-  function handleSelectSquare(){
-    setActivePlayer((curActivePlayer)=>curActivePlayer==='X'?'O':'X');
+  const [gameTurns, setGameTurns]=useState([]);
+  //const [activePlayer, setActivePlayer]=useState('X');
+  const activePlayer=deriveActivePlayer(gameTurns)
+  function handleSelectSquare(rowIndex, colIndex){
+    //setActivePlayer((curActivePlayer)=>curActivePlayer==='X'?'O':'X');
+    setGameTurns((prevTurns)=>{
+      const currentPlayer=deriveActivePlayer(prevTurns);
+      const updatedTurns=[
+        { square:{row: rowIndex, col:colIndex}, player:currentPlayer},...prevTurns,]
+      return updatedTurns;
+    })
   }
   return (
     <main>
@@ -16,8 +32,13 @@ function App() {
           <Player initalName="Player-1" symbol="X" isActive={activePlayer==='X'}/>
           <Player initalName="Player-2" symbol="O" isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard 
+        onSelectSquare={handleSelectSquare} 
+        activePlayerSymbol={activePlayer}
+        turns={gameTurns} 
+        />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
